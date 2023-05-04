9f7c8608c703eb024d309ceb9cef8f6b2e49a21a
zjleblanc <zjleblanc3@gmail.com>
2023-05-03 19:35:32 -0500

    elevate for rhsm and update hook script

--- scripts/cronjobs/ansible-hook.sh
+++ scripts/cronjobs/ansible-hook.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 ipv4=$(ifconfig eth0 | grep 192 | awk '{$1=$1};1' | cut -d ' ' -f 2)
-echo $ipv4 > ~/ip.txt
+mac=$(ifconfig | grep ether | awk '{$1=$1};1' | cut -d ' ' -f 2)
+mac_short=$(echo $mac | sed '/s/://g')
 
-echo '{"name": "'$ipv4'", "inventory": 7, "description": "provisioned via ansible", "variables": "---\nansible_host: '$ipv4'\nansible_user: cloud_user\n"}' > host.json
+echo '{"name": "rhel-guest-'$mac_short'", "inventory": 7, "description": "provisioned via ansible", "variables": "---\nansible_host: '$ipv4'\nansible_user: cloud_user\nguest_mac_address: '$mac'\n"}' > host.json
 
 if test -f "/.aap"; then
-        token=$(cat /.aap)
+    token=$(cat /.aap)
 	curl -v -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d @host.json https://controller.autodotes.com/api/v2/hosts/
-
 	rm -f /.aap
 fi
