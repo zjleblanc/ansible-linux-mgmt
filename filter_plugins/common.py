46c4658f11ee040fc56c6a2b2758179854c6dddf
Zachary LeBlanc <zjleblanc3@gmail.com>
2024-09-06 11:51:30 -0500

    add user login report data generation playbook

--- filter_plugins/common.py
+++ filter_plugins/common.py
@@ -7,8 +7,39 @@ class FilterModule(object):
     def filters(self):
         return {
             'parse_usernames': self.do_parse_usernames,
-            'random_sample': self.do_random_sample
+            'random_sample': self.do_random_sample,
+            'last_reportify': self.do_last_reportify
         }
+
+    ## filters ##
+
+    def do_parse_usernames(self, usernames):
+        user_objects = map(self.__create_user_obj, usernames)
+        return {"campers": list(user_objects)}
+
+    def do_random_sample(self, choices, size=1):
+        random.seed(a=None, version=2)
+        return random.sample(choices, size)
+
+    def do_last_reportify(self, last, homedirs):
+        post_sort = sorted(last, key = lambda x: (x['user'], x['login_epoch']), reverse=True)
+        post_filter = filter(lambda rec: rec['user'] != 'zach' and rec['user'] != 'reboot', post_sort)
+
+        def __map_last_record(record):
+            import datetime
+            return {
+                "user": record['user'],
+                "hostname": record['hostname'],
+                "login": datetime.datetime.fromtimestamp(record['login_epoch']).strftime('%Y-%m-%d %H:%M:%S'),
+                "logout": "" if 'logout_epoch' not in record else datetime.datetime.fromtimestamp(record['logout_epoch']).strftime('%Y-%m-%d %H:%M:%S'),
+                "duration": "" if 'duration' not in record else record['duration'],
+                "tty": record['tty'],
+                "homedir_size": homedirs[record['user']].split('\t')[0]
+            }
+        post_map = map(__map_last_record, post_filter)
+        return list(post_map)
+
+    ## helper functions ##
 
     def __create_user_obj(self, username):
         first_name, last_name = username.split(" ")
@@ -22,11 +53,3 @@ class FilterModule(object):
             "home": "/home/" + username,
             "size_mb": random.choice(RANDOM_SIZES)
         }
-
-    def do_parse_usernames(self, usernames):
-        user_objects = map(self.__create_user_obj, usernames)
-        return {"campers": list(user_objects)}
-
-    def do_random_sample(self, choices, size=1):
-        random.seed(a=None, version=2)
-        return random.sample(choices, size)
