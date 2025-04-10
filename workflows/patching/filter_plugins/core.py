2841e1bdac7a924533632bff94459de577b0213a
Zachary LeBlanc <zjleblanc3@gmail.com>
2025-04-09 21:33:20 -0500

    fix plugin

--- workflows/patching/filter_plugins/core.py
+++ workflows/patching/filter_plugins/core.py
@@ -6,12 +6,15 @@ class FilterModule(object):
         }
 
     def do_path_determination(self, hostvars):
-        groups = {"rhel8_10":[],"rhel9_5":[]}
+        groups = {
+          "rhel8_10": [],
+          "rhel9_5": []
+        }
 
         for host in hostvars:
           if hostvars[host]['ansible_distribution_version'] == '8.10':
-            groups["rhel8_10"] = groups["rhel8_10"].append(host)
+            groups["rhel8_10"].append(host)
           elif hostvars[host]['ansible_distribution_version'] == '9.5':
-            groups["rhel9_5"] = groups["rhel9_5"].append(host)
+            groups["rhel9_5"].append(host)
 
         return groups
