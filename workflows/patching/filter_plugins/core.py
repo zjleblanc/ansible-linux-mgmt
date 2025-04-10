2f6615a20667be00189ec12c0f9112c1934e2e13
Zachary LeBlanc <zjleblanc3@gmail.com>
2025-04-09 22:04:12 -0500

    pass relevant data to group plugin for processing

--- workflows/patching/filter_plugins/core.py
+++ workflows/patching/filter_plugins/core.py
@@ -5,17 +5,20 @@ class FilterModule(object):
             "group_by_path": self.do_path_determination
         }
 
-    def do_path_determination(self, hostvars):
+    def do_path_determination(self, dispatch_host_metadata):
         groups = {
-          "rhel8_10": [],
-          "rhel9_5": []
+          "rhel8_10": {},
+          "rhel9_5": {}
         }
 
-        for host in hostvars:
-          version = hostvars[host]['ansible_facts']['distribution_version']
+        for host in dispatch_host_metadata:
+          version = host['version']
+          name = host['inventory_hostname']
           if version == '8.10':
-            groups["rhel8_10"].append(host)
+            groups["rhel8_10"].setdefault('patch_targets', []).append(name)
+            groups["rhel8_10"].setdefault('patch_targets_metadata', {})[name] = host
           elif version == '9.5':
-            groups["rhel9_5"].append(host)
+            groups["rhel9_5"].setdefault('patch_targets', []).append(name)
+            groups["rhel9_5"].setdefault('patch_targets_metadata', {})[name] = host
 
         return groups
